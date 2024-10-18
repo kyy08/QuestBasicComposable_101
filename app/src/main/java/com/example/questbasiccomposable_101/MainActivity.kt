aca4d43d107256effb2beea964c5408a7376d873
kyy08 <kyyelzz1@gmail.com>
2024-10-18 07:39:29 +0700

    Kode ini umumnya akan digunakan dalam aktivitas utama dari aplikasi Android yang memanfaatkan Jetpack Compose untuk merender UI dengan gaya modern dan reaktif.

--- app/src/main/java/com/example/questbasiccomposable_101/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_101/MainActivity.kt
@@ -1,3 +1,25 @@
 package com.example.questbasiccomposable_101
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.questbasiccomposable_101.ui.theme.QuestBasicComposable_101Theme
 
